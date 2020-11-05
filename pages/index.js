cedbecc4270e42bdaca35e2279e5ec7070ad0916
Somto M.Ugeh <smugeh@gmail.com>
2020-11-05 19:43:39 +0100

    chore: Added env to dev on vercel

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import TopTracks from '@/components/TopTracks';
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
@@ -20,33 +21,7 @@ export default function Home() {
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          <TopTracks />
         </div>
       </main>
 
@@ -61,5 +36,5 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  )
+  );
 }
